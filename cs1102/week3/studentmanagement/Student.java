77d1b806659704b88d9ac1b3b9cc0c599d584fbb
Mathius Edwin Johnson <60864543+yogimathius@users.noreply.github.com>
2024-12-04 22:10:05 -0800

    feat: add update methods to Student

--- cs1102/week3/studentmanagement/Student.java
+++ cs1102/week3/studentmanagement/Student.java
@@ -21,11 +21,23 @@ public class Student {
     return name;
   }
 
+  public void setName(String name) {
+    this.name = name;
+  }
+
   public int getAge() {
     return age;
   }
 
+  public void setAge(int age) {
+    this.age = age;
+  }
+
   public String getGrade() {
     return grade;
+  }
+
+  public void setGrade(String grade) {
+    this.grade = grade;
   }
 }
