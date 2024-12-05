08dfd73a30b163b5ceda6ae5eebf6e0a3da15e1d
Mathius Edwin Johnson <60864543+yogimathius@users.noreply.github.com>
2024-12-04 21:55:07 -0800

    chore: add getters to Student.java

--- cs1102/week3/studentmanagement/Student.java
+++ cs1102/week3/studentmanagement/Student.java
@@ -1,5 +1,31 @@
 package cs1102.week3.studentmanagement;
 
 public class Student {
-  
+  private int id;
+  private String name;
+  private int age;
+  private String grade;
+
+  public Student(int id, String name, int age, String grade) {
+    this.id = id;
+    this.name = name;
+    this.age = age;
+    this.grade = grade;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public String getGrade() {
+    return grade;
+  }
 }
